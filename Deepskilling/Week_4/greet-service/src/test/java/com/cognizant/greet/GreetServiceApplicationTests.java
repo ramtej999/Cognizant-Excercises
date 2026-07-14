8792e32718d750f86ecbac5b4f0392c15510b122
ramtej999 <ramtejmaturi2005@gmail.com>
2026-07-14 16:46:44 +0530

    Added Week_4 Microservices with API Gateway exercises

---
+++ Deepskilling/Week_4/greet-service/src/test/java/com/cognizant/greet/GreetServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cognizant.greet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreetServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
